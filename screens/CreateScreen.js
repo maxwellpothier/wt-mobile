2148f8fb99bc3dde08a3baca2139fc475ed12264
maxwellpothier <pothiermaxwell@gmail.com>
2023-04-19 08:20:44 -0600

    Add content to the create screen

--- screens/CreateScreen.js
+++ screens/CreateScreen.js
@@ -1,11 +1,35 @@
 import React from "react";
-import {StyleSheet, Text, View} from "react-native";
+import {StyleSheet, Text, View, Image} from "react-native";
+import {Button} from "react-native-elements";
+import {ScrollView, TextInput} from "react-native-gesture-handler";
 
 export default function CreateScreen() {
 	return (
-		<View style={styles.container}>
-			<Text style={styles.title}>Create Screen</Text>
-		</View>
+		<ScrollView>
+			<View style={styles.container}>
+				<Image
+					source={{
+						uri: "https://i.discogs.com/jCf-CAJzYPbEsIGjuZQ-5titKZrqsYRN6mS9Woxhf_s/rs:fit/g:sm/q:90/h:525/w:600/czM6Ly9kaXNjb2dz/LWRhdGFiYXNlLWlt/YWdlcy9SLTQ1NzIx/LTEzOTQ4MjY2NTMt/MjA5OC5qcGVn.jpeg",
+					}}
+					style={styles.image}
+				/>
+				<Text style={styles.title}>How'd you like the album?</Text>
+				<TextInput
+					style={styles.input}
+					keyboardType="numeric"
+					// value={numberInput}
+					// onChangeText={handleNumberInputChange}
+				/>
+				<TextInput
+					style={styles.input}
+					multiline={true}
+					numberOfLines={4}
+					// value={textInput}
+					// onChangeText={handleTextInputChange}
+				/>
+				<Button title="Submit" />
+			</View>
+		</ScrollView>
 	);
 }
 
@@ -18,5 +42,18 @@ const styles = StyleSheet.create({
 	title: {
 		fontSize: 24,
 		fontWeight: "bold",
+	},
+	image: {
+		width: 200,
+		height: 200,
+		marginBottom: 20,
+	},
+	input: {
+		borderWidth: 1,
+		borderColor: "#ccc",
+		borderRadius: 5,
+		padding: 10,
+		marginBottom: 20,
+		width: "100%",
 	},
 });
